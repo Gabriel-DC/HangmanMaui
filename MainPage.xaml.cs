ca2642d7b3357d3535eedbd2445fdcec138bf38f
Gabriel Almeida <57327840+Gabriel-DC@users.noreply.github.com>
2023-11-16 19:48:30 -0300

    HangmanMaui

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,25 +1,64 @@
-﻿namespace HangmanMaui
+﻿using HangmanMaui.Models;
+using System.Diagnostics;
+
+namespace HangmanMaui
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        private readonly HangmanContext _context;
+        private readonly List<Button> _buttons = [];
 
         public MainPage()
         {
             InitializeComponent();
+            _context = new HangmanContext();
+            BindingContext = _context;
+            DesignKeyBoard();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        public void DesignKeyBoard()
         {
-            count++;
+            for (char letter = 'A'; letter <= 'Z'; letter++)
+                _buttons.Add(new Button() { Text = letter.ToString(), HeightRequest = 50, Margin = 10 });
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            foreach (Button button in _buttons)
+            {
+                button.Clicked += Button_Clicked;
+                FlexLayoutButtons.Children.Add(button);
+            }
         }
+
+        public async void Button_Clicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string? letter = button.Text;
+
+            if (letter is null || _context.ErrorsCount >= 6)
+                return;
+
+            button.IsEnabled = false;
+            _context.NewCharInput(char.Parse(letter!));
+
+            if(_context.ErrorsCount >= 6)
+            {
+                await DisplayAlert("Você Perdeu!", $"A palavra correta era: {_context.GetSelectedWord()}", "OK");
+                return;
+            }
+
+            if(_context.GetSelectedWord().Equals(_context.CurrentWord, StringComparison.OrdinalIgnoreCase))
+            {
+                await DisplayAlert("Você Acertou!!", $"Parabéns!!", "OK");
+                return;
+            }
+        }
+
+        private void BtnResetGame_Clicked(object sender, EventArgs e)
+        {
+            _buttons.ForEach(button => button.IsEnabled = true);
+            _context.ReloadGame();
+        }
+
+
     }
 
 }
---
+++ Models/HangmanContext.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanMaui.Models
+{
+    public class HangmanContext : INotifyPropertyChanged
+    {
+        private List<string> _availableWords = new List<string>()
+        {
+            "gabriel", "bochechas", "tela", "notebook", "castelo", "maui"
+        }.ConvertAll(word => word.ToUpper());
+
+        private string _selectedWord;
+        private List<char> _usedChars;
+
+        private int _errorCount;
+
+        public int ErrorsCount
+        {
+            get => _errorCount;
+            set
+            {
+                _errorCount = value;
+                OnPropertyChanged();
+            }
+        }        
+
+        private string _currentWord;
+
+        public string CurrentWord
+        {
+            get => _currentWord;
+            set
+            {
+                _currentWord = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public HangmanContext()
+        {
+            InitGame();
+        }
+
+        public void InitGame() => ReloadGame();
+
+        public void ReloadGame()
+        {
+            ErrorsCount = 0;
+            _selectedWord = _availableWords[Random.Shared.Next(_availableWords.Count - 1)];
+            CurrentWord = string.Join("", _selectedWord.ToList().ConvertAll(character => "_"));
+            _usedChars = [];
+        }
+
+        public void NewCharInput(char input)
+        {
+            char newInput = char.ToUpper(input);
+
+            if(_usedChars.Contains(newInput) || _errorCount > 6) 
+                return;
+
+            _usedChars.Add(newInput);
+
+            if(_selectedWord.Contains(newInput, StringComparison.OrdinalIgnoreCase))
+            {
+                ReplaceAllOcurrences(newInput);
+                return;
+            }
+
+            ErrorsCount++;
+        }
+
+        private void ReplaceAllOcurrences(char newChar, int index = 0)
+        {
+            int charIndex = _selectedWord.IndexOf(newChar, index);
+
+            if(charIndex > -1)
+            {
+                char[] charArr = CurrentWord.ToCharArray();
+                
+                charArr[charIndex] = newChar;
+
+                CurrentWord = new string(charArr);
+
+                ReplaceAllOcurrences(newChar, charIndex + 1);
+            }
+        }
+
+        public string GetSelectedWord() => _selectedWord;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
